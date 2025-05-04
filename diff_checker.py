8eb9466c008831265ebb9dadddc6a91a453e0e37
Addysaurus <152833023+Addysaurus@users.noreply.github.com>
2025-05-04 16:56:38 -0400

    1.12.04d8

--- diff_checker.py
+++ diff_checker.py
@@ -19,6 +19,6 @@ def check_diff(old_file, new_file, output_file):
             if i[0] not in [j[0] for j in old_entries]:
                 writer.writerow(i)
             
-check_diff('DEV_units.csv', 'DEV2_units.csv', 'DIFF_units.csv')
-check_diff('DEV_units_weaponry.csv', 'DEV2_units_weaponry.csv', 'DIFF_units_weaponry.csv')
-check_diff('DEV_units_modifications.csv', 'DEV2_units_modifications.csv', 'DIFF_units_modifications.csv')
+check_diff('DEV/DEV_units.csv', 'DEV2/DEV2_units.csv', 'DIFF/DIFF_units.csv')
+check_diff('DEV/DEV_units_weaponry.csv', 'DEV2/DEV2_units_weaponry.csv', 'DIFF/DIFF_units_weaponry.csv')
+check_diff('DEV/DEV_units_modifications.csv', 'DEV2/DEV2_units_modifications.csv', 'DIFF/DIFF_units_modifications.csv')

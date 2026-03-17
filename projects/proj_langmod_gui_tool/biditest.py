7a4b8d1f0b242b040725a277f8e3b197f2988a0d
Addysaurus <addisonalmagor@gmail.com>
2026-03-17 16:09:24 -0400

    further simplifying bidi test

--- projects/proj_langmod_gui_tool/biditest.py
+++ projects/proj_langmod_gui_tool/biditest.py
@@ -3,7 +3,5 @@ import re
 t = "מרכז שיקום ואחזקה 7000 / רפאל מערכות לחימה מתקדמות | (Samson Remote Controlled Weapon Station 30mm) נגמ״ש מרכבה"
 list1 = re.split(r'([\u0591-\u07FF \s?]+)\s', t)
 list2 = [i.strip()[::-1] if re.match(r'[\u0591-\u07FF \s?]', i) else i for i in list1]
-for i in list2:
-    if i == '':
-        list2.pop(list2.index(i))
+[list2.pop(list2.index(i)) for i in list2 if i == '']
 print(' '.join(list2))

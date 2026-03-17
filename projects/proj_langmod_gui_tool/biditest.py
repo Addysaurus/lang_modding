e43599641ee53e62b6a1a085b3ccdfb3c7b2798e
Addysaurus <addisonalmagor@gmail.com>
2026-03-17 16:06:42 -0400

    simplifying bidi overwrite stuff

--- projects/proj_langmod_gui_tool/biditest.py
+++ projects/proj_langmod_gui_tool/biditest.py
@@ -2,9 +2,8 @@ import re
 
 t = "מרכז שיקום ואחזקה 7000 / רפאל מערכות לחימה מתקדמות | (Samson Remote Controlled Weapon Station 30mm) נגמ״ש מרכבה"
 list1 = re.split(r'([\u0591-\u07FF \s?]+)\s', t)
-list2 = [i.strip() for i in list1]
-list3 = [i[::-1] if re.match(r'[\u0591-\u07FF \s?]', i) else i for i in list2]
-for i in list3:
+list2 = [i.strip()[::-1] if re.match(r'[\u0591-\u07FF \s?]', i) else i for i in list1]
+for i in list2:
     if i == '':
-        list3.pop(list3.index(i))
-print(' '.join(list3))
+        list2.pop(list2.index(i))
+print(' '.join(list2))
